1c24cbd20540a4bbd51dc6ce850961b07d1aaeea
Edmund Yap <edmundyjl0505@gmail.com>
2026-01-07 16:08:31 +0100

    Implemented Player's movement and jumping
    Create: Player's prefab, Platform's prefab and Player's script (x-axis movement and jumping mechanism)

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,74 @@
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class Player : MonoBehaviour
+{
+    [Header("Movement")]
+    public float moveSpeed = 8f;
+    public float jumpForce = 8f;
+    public float fallMultiplier = 2f;
+    public float riseMultiplier = 2f;
+
+    [Header("Ground Check")]
+    public Transform groundCheck;
+    public float groundCheckRadius = 0.2f;
+    public LayerMask groundLayer;
+
+    private Rigidbody rb;
+    private bool isGrounded;
+
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        HandleJump();
+    }
+
+    void FixedUpdate()
+    {
+        HandleMovement();
+        BetterJump();
+        CheckGround();
+    }
+
+    void HandleMovement()
+    {
+        float xInput = Input.GetAxis("Horizontal");
+
+        Vector3 velocity = rb.linearVelocity;
+        velocity.x = xInput * moveSpeed;
+        rb.linearVelocity = velocity;
+    }
+
+    void HandleJump()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+        }
+    }
+
+    void BetterJump()
+    {
+        if (rb.linearVelocity.y < 0)
+        {
+            rb.linearVelocity += Vector3.up * Physics.gravity.y * (fallMultiplier - 1) * Time.fixedDeltaTime;
+        }
+        else if (rb.linearVelocity.y > 0)
+        {
+            rb.linearVelocity += Vector3.up * Physics.gravity.y * (riseMultiplier - 1) * Time.fixedDeltaTime;
+        }
+    }
+
+    void CheckGround()
+    {
+        isGrounded = Physics.CheckSphere(
+            groundCheck.position,
+            groundCheckRadius,
+            groundLayer
+        );
+    }
+}
