b74de650d2dcadd6de4d25c5d35067a2fabbceba
Edmund Yap <edmundyjl0505@gmail.com>
2026-01-15 23:59:55 +0100

    Replace grey-box with actual model, fix MovingObstacle, Rotator script.

--- Assets/Scripts/MovingObstacle.cs
+++ Assets/Scripts/MovingObstacle.cs
@@ -5,17 +5,17 @@ public class MovingObstacle : MonoBehaviour
     [SerializeField] private float moveDistance = 0.9f; //Calculation: 1 - (obstacle scale / parent scale)
     [SerializeField] private float moveSpeed = 2f;
 
-    private Vector3 startLocalPos;
+    private Vector3 startWorldPos;
 
     void Start()
     {
-        startLocalPos = transform.localPosition;
-        startLocalPos.x = 0;
+        startWorldPos = transform.position;
+        startWorldPos.x = 0;
     }
 
     void Update()
     {
         float offset = Mathf.PingPong(Time.time * moveSpeed, moveDistance) - (moveDistance / 2);
-        transform.localPosition = startLocalPos + new Vector3(offset, 0, 0);
+        transform.position = startWorldPos + new Vector3(offset, 0, 0);
     }
 }
